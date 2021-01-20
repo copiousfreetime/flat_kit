fcf0c1cbfa4d14afb48adbc3ea779ac601cbda40
copiousfreetime <jeremy@copiousfreetime.org>
2021-01-20 15:37:16 -0700

    writing to files and gzip files working

--- lib/flat_kit/output/file.rb
+++ lib/flat_kit/output/file.rb
@@ -1,7 +1,10 @@
+require 'zlib'
+
 module FlatKit
   class Output
     class File < Output
       attr_reader :path
+      attr_reader :count
 
       def self.handles?(obj)
         return true if obj.instance_of?(Pathname)
@@ -15,24 +18,44 @@ module FlatKit
       end
 
       def initialize(obj)
+        @count = 0
         @path = Pathname.new(obj)
         path.dirname.mkpath
-        @io = path.open("w")
+        @io = open_output(path)
       end
 
       def name
         path.to_s
       end
 
+      def write(obj)
+        rc = @io.write(obj.to_s)
+        @count += 1
+        rc
+      end
+
       def close
         @io.close
       end
 
-      # internal api method
+      # internal api method for testing purposes
       def io
         @io
       end
 
+      private
+
+      # open the opropriate otuput type depending on the destination file name
+      #
+      # TODO: add in bzip
+      def open_output(path)
+        case path.extname
+        when ".gz"
+          Zlib::GzipWriter.open(path.to_s)
+        else
+          path.open("wb")
+        end
+      end
     end
   end
 end
--- test/output/test_file.rb
+++ test/output/test_file.rb
@@ -28,5 +28,35 @@ module TestOutput
         File.unlink(test_path) if File.exist?(test_path)
       end
     end
+
+    def test_writes_to_file
+      test_path = "tmp/test_writes_to_file.txt"
+      begin
+        output = ::FlatKit::Output::File.new(test_path)
+        assert_equal(test_path, output.name)
+        output.write("test_writes_to_file output")
+        output.close
+        t = IO.read(test_path)
+        assert_equal("test_writes_to_file output", t)
+        assert_equal(1, output.count)
+      ensure
+        File.unlink(test_path) if File.exist?(test_path)
+      end
+    end
+
+    def test_writes_to_gzfile
+      test_path = "tmp/test_writes_to_gzfile.txt.gz"
+      begin
+        output = ::FlatKit::Output::File.new(test_path)
+        assert_equal(test_path, output.name)
+        output.write("test_writes_to_gzfile output")
+        output.close
+        t = %x[ gunzip -c #{test_path} ]
+        assert_equal("test_writes_to_gzfile output", t)
+        assert_equal(1, output.count)
+      ensure
+        File.unlink(test_path) if File.exist?(test_path)
+      end
+    end
   end
 end
