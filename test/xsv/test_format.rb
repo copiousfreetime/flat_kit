0991e007444e87aa161ef6eb6c8dceb5e346fe33
copiousfreetime <jeremy@copiousfreetime.org>
2021-01-21 11:34:12 -0700

    more hanles format tests

--- test/xsv/test_format.rb
+++ test/xsv/test_format.rb
@@ -6,5 +6,17 @@ module TestXsv
     def test_handles_csv
       assert(::FlatKit::Xsv::Format.handles?("csv"))
     end
+
+    def test_handles_tsv
+      assert(::FlatKit::Xsv::Format.handles?("tsv"))
+    end
+
+    def test_handles_txt
+      assert(::FlatKit::Xsv::Format.handles?("txt"))
+    end
+
+    def test_does_not_handle_json
+      refute(::FlatKit::Xsv::Format.handles?("json"))
+    end
   end
 end
