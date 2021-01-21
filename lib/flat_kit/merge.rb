d75d7e80c448ac5bd149ce329c5cf7b67fdb844c
copiousfreetime <jeremy@copiousfreetime.org>
2021-01-20 21:16:46 -0700

    switch to compare keys field

--- lib/flat_kit/merge.rb
+++ lib/flat_kit/merge.rb
@@ -1,20 +1,25 @@
 module FlatKit
   class Merge
+    attr_reader :compare_keys
     attr_reader :inputs
-    attr_reader :key
     attr_reader :output
     attr_reader :logger
     attr_reader :output_format
 
-    def initialize(inputs:, key:, output: $stdout,
-                   logger: FlatKit.logger, output_format: :json)
-      @inputs  = inputs.map { |i| FlatKit::Input.from(i) }
-      @key     = key
-      @readers = @inputs.map { |i| FlatKit::Reader.for(i).new(input: i, key: key) }
+    def initialize(inputs:, compare_keys:, input_format: :auto,
+                   output: $stdout, output_format: :auto,
+                   logger: FlatKit.logger)
 
-      @output = FlatKit::Output.from(output)
+      @compare_keys = compare_keys.map{ |k| k.to_s }
+      @inputs       = inputs
+      @input_format = input_format
+
+      #@readers = inputs.map { |r| FlatKit::Reader.new(source: i, compare_keys: compare_keys, format: input_format) }
+
       @output_format = output_format
-      #@writer = Writer.new(output: @output, format: @output_format)
+      @output = FlatKit::Output.from(output)
+      #@writer = FlatKit::Writer.new(destination: output, format: output_format)
+
       @logger = logger
     end
 
--- test/test_merge.rb
+++ test/test_merge.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class TestMerge < ::Minitest::Test
   def test_can_use_stdout_as_output
-    merge = ::FlatKit::Merge.new(inputs: [], output: $stdout, key: [])
+    merge = ::FlatKit::Merge.new(inputs: [], output: $stdout, compare_keys: [])
     assert_match(/STDOUT/, merge.output.name)
     assert_instance_of(::FlatKit::Output::IO, merge.output)
   end
 
   def test_can_use_use_dash_as_output
-    merge = ::FlatKit::Merge.new(inputs: [], output: "-", key: [])
+    merge = ::FlatKit::Merge.new(inputs: [], output: "-", compare_keys: [])
     assert_match(/STDOUT/, merge.output.name)
     assert_instance_of(::FlatKit::Output::IO, merge.output)
   end
@@ -17,7 +17,7 @@ class TestMerge < ::Minitest::Test
     test_path = "test_can_use_a_file_as_output.txt"
     begin
       File.open(test_path, "w") do |f|
-        merge = ::FlatKit::Merge.new(inputs: [], output: f, key: [])
+        merge = ::FlatKit::Merge.new(inputs: [], output: f, compare_keys: [])
         assert_equal(test_path, merge.output.name)
         assert_instance_of(::FlatKit::Output::IO, merge.output)
       end
@@ -29,29 +29,29 @@ class TestMerge < ::Minitest::Test
   def test_can_use_a_text_path_as_output
     test_path = "tmp/test_can_use_a_text_path_as_output.txt"
     begin
-      merge = ::FlatKit::Merge.new(output: test_path, inputs: [], key: [])
+      merge = ::FlatKit::Merge.new(output: test_path, inputs: [], compare_keys: [])
       assert_equal(test_path, merge.output.name)
       assert_instance_of(::FlatKit::Output::File, merge.output)
       merge.output.close
     ensure
-      File.unlink(test_path)
+      File.unlink(test_path) if File.exist?(test_path)
     end
   end
 
   def test_can_use_a_pathname_as_output
     test_path = Pathname.new("tmp/test_can_use_a_pathname_as_output.txt")
     begin
-      merge = ::FlatKit::Merge.new(output: test_path, inputs: [], key: [])
+      merge = ::FlatKit::Merge.new(output: test_path, inputs: [], compare_keys: [])
       assert_equal(test_path.to_s, merge.output.name)
       assert_instance_of(::FlatKit::Output::File, merge.output)
       merge.output.close
     ensure
-      test_path.unlink
+      test_path.unlink if test_path.exist?
     end
   end
 
   def test_raises_error_if_unable_to_parse_output
     test_path = Object.new
-    assert_raises(FlatKit::Error) { ::FlatKit::Merge.new(output: test_path, inputs: [], key: []) }
+    assert_raises(FlatKit::Error) { ::FlatKit::Merge.new(output: test_path, inputs: [], compare_keys: []) }
   end
 end
