1c815e95223fa60d8f974c2c12fa114e9d15f92d
copiousfreetime <jeremy@copiousfreetime.org>
2021-01-21 13:28:14 -0700

    test looking up formats

---
+++ test/test_format.rb
@@ -0,0 +1,13 @@
+require_relative 'test_helper'
+
+class TestFormat < ::Minitest::Test
+  def test_finds_jsonl_format
+    klass = ::FlatKit::Format.for("data.json.gz")
+    assert_equal(::FlatKit::Jsonl::Format, klass)
+  end
+
+  def test_finds_xsv_format
+    klass = ::FlatKit::Format.for("data.csv.gz")
+    assert_equal(::FlatKit::Xsv::Format, klass)
+  end
+end
