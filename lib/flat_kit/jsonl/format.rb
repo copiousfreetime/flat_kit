c7f9cc177b1ce49eeae48cc290daf80750203499
copiousfreetime <jeremy@copiousfreetime.org>
2021-01-21 13:24:09 -0700

    test the jsonl format handler

--- lib/flat_kit/jsonl/format.rb
+++ lib/flat_kit/jsonl/format.rb
@@ -1,7 +1,13 @@
 module FlatKit
   module Jsonl
     class Format < ::FlatKit::Format
-
+      def self.handles?(filename)
+        parts = filename.split(".")
+        %w[ json jsonl ndjson ].each do |ext|
+          return true if parts.include?(ext)
+        end
+        return false
+      end
     end
   end
 end
---
+++ test/jsonl/test_format.rb
@@ -0,0 +1,22 @@
+require_relative '../test_helper'
+
+module TestJsonl
+  class TestFormat < ::Minitest::Test
+
+    def test_handles_json
+      assert(::FlatKit::Jsonl::Format.handles?("data.json.gz"))
+    end
+
+    def test_handles_jsonl
+      assert(::FlatKit::Jsonl::Format.handles?("data.jsonl"))
+    end
+
+    def test_handles_ndjson
+      assert(::FlatKit::Jsonl::Format.handles?("log.ndjson"))
+    end
+
+    def test_does_not_handle_csv
+      refute(::FlatKit::Jsonl::Format.handles?("data.csv"))
+    end
+  end
+end
