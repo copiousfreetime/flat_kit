59b6a00286eaef2dc4b58dea8ff1fdff49de623a
copiousfreetime <jeremy@copiousfreetime.org>
2021-02-25 10:30:45 -0700

    Add streaming active record to csv using flatkit

---
+++ examples/stream-active-record-to-csv.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env
+
+#------------------------------------------------------------------------------
+# This is an example to show how to stream an active record scope to a CSV file
+# using FlatKit.
+#------------------------------------------------------------------------------
+
+require 'flat_kit'     # gem 'flat_kit'
+require 'progress_bar' # gem 'progress-bar'
+
+# get an appropriate scope from one of your models - or any scope for that
+# matter
+scope      = MyActiveRecordModel.all
+
+# Output to a file that is csv, and automatically gzipped
+#
+output_csv = ::FlatKit::Xsv::Writer.new(destination: "export.csv.gz")
+
+# handy progress bar
+bar        = ProgressBar.new(scope.count)
+
+# using active record in batches to not pull all the recors from the database at
+# once
+#
+#  https://api.rubyonrails.org/classes/ActiveRecord/Batches.html#method-i-in_batches
+scope.in_batches do |batch|
+
+  batch.each do |record|
+
+    # generate an XSV Record by pulling hte attributes out of the active record
+    # model. You may also want to generate a hash from a query or something
+    # along those lines. In any case pass in a Hash to complete_structured_data:
+    # and nil to data.
+    xsv_record = ::FlatKit::Xsv::Record.new(data: nil, complete_structured_data: record.attributes)
+
+    # FlatKit will automatically handle writing out the header line based upon
+    # the fields in the first record.
+    output_csv.write(xsv_record)
+
+    bar.increment!
+  end
+end
+
+# close the output file explicitly
+output_csv.close
