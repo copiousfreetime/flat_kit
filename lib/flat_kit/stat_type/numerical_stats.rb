4932e0a11320f6ad5652e3afb94a14b7985875f2
copiousfreetime <jeremy@copiousfreetime.org>
2021-03-01 10:10:49 -0700

    adding in numerical stats

---
+++ lib/flat_kit/stat_type/numerical_stats.rb
@@ -0,0 +1,119 @@
+#--
+# Copyright (c) 2008, 2009 Jeremy Hinegardner
+# All rights reserved.  See LICENSE and/or COPYING for details.
+#
+# Pulled from Hitimes, which I also wrote
+#++
+
+require 'thread'
+require 'oj'
+
+module FlatKit
+  class StatType
+    #
+    # Stats object will keep track of the _min_, _max_, _count_, _sum_ and _sumsq_ 
+    # and when you want you may also retrieve the _mean_, _stddev_ and _rate_.
+    #
+    # this contrived example shows getting a list of all the files in a directory
+    # and running stats on file sizes.
+    #
+    #     s = FlatKit::Stats.new
+    #     dir = ARGV.shift || Dir.pwd
+    #     Dir.entries( dir ).each do |entry|
+    #       fs = File.stat( entry )
+    #       if fs.file? then
+    #         s.update( fs.size )
+    #        end
+    #     end
+    #
+    #     %w[ count min max mean sum stddev rate ].each do |m|
+    #       puts "#{m.rjust(6)} : #{s.send( m ) }"
+    #     end
+    #
+    class NumericalStats < StatType
+      # A list of the available stats
+
+      attr_reader :min
+      attr_reader :max
+      attr_reader :count
+      attr_reader :sum
+      attr_reader :sumsq
+
+      def self.default_stats
+        @default_stats ||= %w[ count max mean min rate stddev sum sumsq ]
+      end
+
+      def initialize
+        @mutex = Mutex.new
+        @min = Float::INFINITY
+        @max = -Float::INFINITY
+        @count = 0
+        @sum = 0.0
+        @sumsq = 0.0
+      end
+
+      def collected_stats
+        return self.class.default_stats
+      end
+
+      # call-seq:
+      #    stat.update( val ) -> val
+      #
+      # Update the running stats with the new value.
+      # Return the input value.
+      def update(value)
+        @mutex.synchronize do
+          @min = (value < @min) ? value : @min
+          @max = (value > @max) ? value : @max
+
+          @count += 1
+          @sum   += value
+          @sumsq += (value * value)
+        end
+
+        return value
+      end
+
+      # call-seq:
+      #    stat.mean -> Float
+      # 
+      # Return the arithmetic mean of the values put into the Stats object.  If no
+      # values have passed through the stats object then 0.0 is returned;
+      def mean
+        return 0.0 if @count.zero?
+        return @sum / @count
+      end
+
+      # call-seq:
+      #    stat.rate -> Float
+      #
+      # Return the +count+ divided by +sum+.
+      #
+      # In many cases when Stats#update( _value_ ) is called, the _value_ is a unit
+      # of time, typically seconds or microseconds.  #rate is a convenience for those
+      # times.  In this case, where _value_ is a unit if time, then count divided by
+      # sum is a useful value, i.e. +something per unit of time+.
+      #
+      # In the case where _value_ is a non-time related value, then the value
+      # returned by _rate_ is not really useful.
+      #
+      def rate
+        return 0.0 if @sum.zero?
+        return @count / @sum
+      end
+
+      #
+      # call-seq:
+      #    stat.stddev -> Float
+      #
+      # Return the standard deviation of all the values that have passed through the
+      # Stats object.  The standard deviation has no meaning unless the count is > 1,
+      # therefore if the current _stat.count_ is < 1 then 0.0 will be returned;
+      #
+      def stddev
+        return 0.0 unless @count > 1
+        Math.sqrt((@sumsq - ((@sum * @sum)/@count)) / (@count - 1))
+      end
+    end
+  end
+end
---
+++ test/stat_type/test_numerical_stats.rb
@@ -0,0 +1,95 @@
+require_relative '../test_helper'
+
+
+module TestStatType
+  class TestNumericalStats < ::Minitest::Test
+    def setup
+      @stats = FlatKit::StatType::NumericalStats.new
+      @full_stats = FlatKit::StatType::NumericalStats.new
+      [ 1, 2, 3].each { |i| @full_stats.update( i ) }
+    end
+
+    def test_intialized_with_usable_values
+      assert_equal(0, @stats.count)
+      assert_equal(Float::INFINITY, @stats.min)
+      assert_equal(-Float::INFINITY, @stats.max)
+      assert_equal(0.0, @stats.sum)
+      assert_equal(0.0, @stats.rate)
+    end
+
+    def test_calculates_mean
+      assert_equal(2.0, @full_stats.mean)
+    end
+
+    def test_calculates_rate
+      assert_equal(0.5, @full_stats.rate)
+    end
+
+    def test_tracks_the_maximum_value
+      assert_equal(3.0, @full_stats.max)
+    end
+
+    def test_tracks_the_minimum_value
+      assert_equal(1.0, @full_stats.min)
+    end
+
+    def test_tracks_the_count
+      assert_equal(3,@full_stats.count)
+    end
+
+    def test_tracks_the_sum
+      assert_equal(6.0, @full_stats.sum)
+    end
+
+    def test_calculates_the_standard_deviation
+      assert_equal(1.0, @full_stats.stddev)
+    end 
+
+    def test_calculates_the_sum_of_squares
+      assert_equal(14, @full_stats.sumsq)
+    end 
+
+    def test_converts_to_a_hash
+      h = @full_stats.to_hash
+      assert_equal(::FlatKit::StatType::NumericalStats.default_stats.size, h.size)
+      assert_equal(::FlatKit::StatType::NumericalStats.default_stats, h.keys.sort)
+    end
+
+    def test_converts_to_a_limited_hash_if_given_arguments
+      h = @full_stats.to_hash( "min", "max", "mean" )
+      assert_equal(3, h.size)
+      assert_equal(%w[ max mean min], h.keys.sort)
+
+      h = @full_stats.to_hash( %w[ count rate ] )
+      assert_equal(2, h.size)
+      assert_equal(%w[ count rate ], h.keys.sort)
+    end
+
+    def test_raises_nomethoderror_if_an_invalid_stat_is_used
+      assert_raises(NoMethodError) { @full_stats.to_hash( "wibble" ) }
+    end
+
+    def test_converts_to_a_json_string
+      j = @full_stats.to_json
+      h = JSON.parse( j )
+      assert_equal(::FlatKit::StatType::NumericalStats.default_stats.size, h.size)
+      assert_equal(::FlatKit::StatType::NumericalStats.default_stats, h.keys.sort)
+    end
+
+    def test_converts_to_a_limited_json_hash_if_given_arguments
+      j = @full_stats.to_json( "min", "max", "mean" )
+      h = JSON.parse( j )
+      assert_equal(3, h.size)
+      assert_equal(%w[ max mean min], h.keys.sort)
+
+      j = @full_stats.to_json( %w[ count rate ] )
+      h = JSON.parse( j )
+      assert_equal(2, h.size)
+      assert_equal(%w[ count rate ], h.keys.sort)
+    end
+
+    def test_raises_nomethoderror_if_an_invalid_stat_is_used
+      assert_raises(NoMethodError) { @full_stats.to_json( "wibble" ) }
+    end
+  end
+end
