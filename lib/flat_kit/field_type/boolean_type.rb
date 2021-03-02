1e9c3ff7e6ed5d12a1defc9d470397b54acaf573
copiousfreetime <jeremy@copiousfreetime.org>
2021-03-02 13:52:40 -0700

    test all the boolean coercions

--- lib/flat_kit/field_type/boolean_type.rb
+++ lib/flat_kit/field_type/boolean_type.rb
@@ -14,11 +14,31 @@ module FlatKit
           true
         when String
           REGEX.match?(data)
+        when Integer
+          return true if data.zero?
+          return true if data == 1
+          return false
         else
           false
         end
       end
 
+      def self.coerce(data)
+        case data
+        when TrueClass
+          true
+        when FalseClass
+          false
+        when Numeric
+          return false if data.zero?
+          return true  if data == 1
+          CoerceFailure
+        when String
+          return true  if TRUTHY_REGEX.match?(data)
+          return false if FALSEY_REGEX.match?(data)
+          CoerceFailure
+        end
+      end
     end
   end
 end
--- test/field_type/test_boolean_type.rb
+++ test/field_type/test_boolean_type.rb
@@ -2,6 +2,16 @@ require_relative '../test_helper'
 
 module TestFieldType
   class TestBooleanType < ::Minitest::Test
+    def truthy_items
+      t = %w[ yes Y true t 1 y ]
+      t << 1
+    end
+
+    def falsey_items
+      f = %w[ no n false f 0 N ]
+      f << 0
+    end
+
     def test_true
       assert(FlatKit::FieldType::BooleanType.matches?(true))
     end
@@ -11,19 +21,45 @@ module TestFieldType
     end
 
     def test_truthy_items
-      %w[ yes Y true t 1 y ].each do |s|
+      truthy_items.each do |s|
         assert(FlatKit::FieldType::BooleanType.matches?(s), "#{s} should be boolean")
       end
     end
 
     def test_falsey_items
-      %w[ no n false f 0 ].each do |s|
+      falsey_items.each do |s|
         assert(FlatKit::FieldType::BooleanType.matches?(s), "#{s} should be boolean")
       end
     end
 
     def test_other_class_does_not_match
       refute(FlatKit::FieldType::BooleanType.matches?(Object.new))
+    end
+
+    def test_coerces_falsey_to_boolean
+      falsey_items.each do |t|
+        refute(FlatKit::FieldType::BooleanType.coerce(t))
+      end
+    end
+
+    def test_true_is_truthy
+      assert(FlatKit::FieldType::BooleanType.coerce(true))
+    end
+
+    def test_false_is_falsey
+      refute(FlatKit::FieldType::BooleanType.coerce(false))
+    end
+
+    def test_0_is_false
+      refute(FlatKit::FieldType::BooleanType.coerce(0))
+    end
+
+    def test_1_is_false
+      assert(FlatKit::FieldType::BooleanType.coerce(1))
+    end
+
+    def test_42_is_false
+      assert(FlatKit::FieldType::BooleanType.coerce(42.0))
     end
   end
 end
