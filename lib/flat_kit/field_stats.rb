760d2aa146e899145ad9dcd54e1de5205ae257c5
copiousfreetime <jeremy@copiousfreetime.org>
2021-03-02 17:17:02 -0700

    implement and test field stats

---
+++ lib/flat_kit/field_stats.rb
@@ -0,0 +1,166 @@
+module FlatKit
+  # Collect stats on a single field. We may not know what the field data type is
+  # to start with, so collect a bunch of values until we have the threshold, and
+  # then calculte states based upon the data types determined by the guess
+  #
+  class FieldStats
+    DEFAULT_GUESS_THRESHOLD = 1000
+
+    CORE_STATS = :core
+    CARDINALITY_STATS = :cardinality
+
+    ALL_STATS = [ CORE_STATS, CARDINALITY_STATS ]
+
+    # DEFAULT_STATS = %w[
+    #   count max mean min stddev sum
+    #   null_count percent_nulls
+    #   unknown_count percent_unknown
+    # ].freeze
+
+    attr_reader :type_counts
+    attr_reader :field_type
+
+    def initialize(name:, stats_to_collect: CORE_STATS, type: ::FlatKit::FieldType::GuessType, guess_threshold: DEFAULT_GUESS_THRESHOLD)
+      @name              = name
+      @field_type        = type
+      @guess_threshold   = guess_threshold
+      @type_counts       = Hash.new(0)
+      @out_of_type_count = 0
+      @values            = []
+      @stats             = nil
+      @stats_to_collect  = [stats_to_collect].flatten
+
+      @stats_to_collect.each do |collection_set|
+        next if ALL_STATS.include?(collection_set)
+        raise ArgumentError, "#{collection_set} is not a valid stats collection set, must be one of #{ALL_STATS.map { |s| s.to_s }.join(", ") }"
+      end
+      raise ArgumentError, "type: must be FieldType subclasses - not #{type}" unless type.kind_of?(Class) && (type.superclass == ::FlatKit::FieldType)
+    end
+
+    def field_type_determined?
+      @field_type != ::FlatKit::FieldType::GuessType
+    end
+
+    def update(value)
+      update_type_count(value)
+
+      if field_type_determined? then
+        update_stats(value: value)
+      else
+        @values << value
+
+        if @values.size >= @guess_threshold then
+          resolve_guess
+        end
+      end
+    end
+
+    def collecting_frequencies?
+      @stats_to_collect.include?(CARDINALITY_STATS)
+    end
+
+    def count
+      stats.count
+    end
+
+    def max
+      stats.max if stats.respond_to?(:max)
+    end
+
+    def mean
+      stats.mean if stats.respond_to?(:mean)
+    end
+
+    def min
+      stats.min if stats.respond_to?(:min)
+    end
+
+    def stddev
+      stats.stddev if stats.respond_to?(:stddev)
+    end
+
+    def sum
+      stats.sum if stats.respond_to?(:sum)
+    end
+
+    def mode
+      stats.mode if collecting_frequencies?
+    end
+
+    def unique_count
+      stats.unique_count if collecting_frequencies?
+    end
+
+    def unique_values
+      stats.unique_values if collecting_frequencies?
+    end
+
+    def frequencies
+      stats.frequencies if collecting_frequencies?
+    end
+
+    def null_count
+      type_counts[FieldType::NullType]
+    end
+
+    def total_count
+      stats.count + @out_of_type_count
+    end
+
+    def percent_null
+      return 0 if total_count.zero?
+      null_count.to_f / total_count
+    end
+
+    def unknown_count
+      type_counts[FieldType::UnknownType]
+    end
+
+    def percent_unknown
+      return 0 if total_count.zero?
+      unknown_count.to_f / total_count
+    end
+
+    private
+
+    def stats
+      if !field_type_determined? then
+        resolve_guess
+      end
+      @stats
+    end
+
+    def update_stats(value)
+      coerced_value = @field_type.coerce(value)
+      if coerced_value == FieldType::CoerceFailure then
+        @out_of_type_count += 1
+        return
+      end
+
+      @stats.update(coerced_value)
+      update_cardinality_stats(coerced_value)
+    end
+
+    def update_cardinality_stats(value)
+      if @stats_to_collect.include?(CARDINALITY_STATS) then
+
+      end
+    end
+
+    def update_type_count(value)
+      guess = FieldType.best_guess(value)
+      type_counts[guess] += 1
+      return guess
+    end
+
+    def resolve_guess
+      best_guess_type, _best_guess_count = type_counts.max_by { |k, v| v }
+      @field_type = best_guess_type
+      @stats = StatType.for(@field_type).new(collecting_frequencies: collecting_frequencies?)
+      @values.each do |v|
+        update_stats(v)
+      end
+      @values.clear
+    end
+  end
+end
---
+++ test/test_field_stats.rb
@@ -0,0 +1,130 @@
+require_relative './test_helper'
+
+require 'faker'
+
+class TestFieldStats < ::Minitest::Test
+  # returns [FieldStats, Array] where the array is the original data
+  def generate_data_with(count: 100, stats: ::FlatKit::FieldStats.new(name: "data"), &block)
+    list = Array.new.tap do |a|
+      count.times do
+        n = block.call
+        stats.update(n)
+        a << n
+      end
+    end
+    [stats, list]
+  end
+
+  def test_raises_error_on_invalid_stats
+    assert_raises(ArgumentError) { ::FlatKit::FieldStats.new(name: "test", stats_to_collect: :whatever) }
+  end
+
+  def test_collects_numeric_default_stats
+    field_stats, number_data = generate_data_with { Faker::Number.within(range: 1.0..100.0) }
+
+    null_count = 5
+    null_count.times {
+      field_stats.update(nil)
+    }
+
+    avg = number_data.sum / number_data.size
+    min = number_data.min
+    max = number_data.max
+    sum = number_data.sum
+
+    refute(field_stats.field_type_determined?)
+
+    assert_equal(null_count, field_stats.null_count)
+    assert_equal(number_data.size, field_stats.count)
+
+    assert(field_stats.field_type_determined?)
+
+    assert_in_epsilon(avg, field_stats.mean)
+    assert_equal(min, field_stats.min)
+    assert_equal(max, field_stats.max)
+    assert_in_epsilon(sum, field_stats.sum)
+    assert_in_epsilon(null_count.to_f / (null_count + number_data.size), field_stats.percent_null)
+  end
+
+  def test_collect_numeric_cardinality_stats
+    field_stats = ::FlatKit::FieldStats.new(name: "number-cardinality",
+                                            stats_to_collect: ::FlatKit::FieldStats::ALL_STATS)
+    field_stats, number_data = generate_data_with(stats: field_stats) {
+        Faker::Number.within(range:1..25)
+    }
+
+    avg = number_data.sum.to_f / number_data.size
+    min = number_data.min
+    max = number_data.max
+
+    assert(field_stats.collecting_frequencies?)
+    refute(field_stats.field_type_determined?)
+
+    assert_equal(number_data.size, field_stats.count)
+
+    assert(field_stats.field_type_determined?)
+
+    assert_in_epsilon(avg, field_stats.mean)
+    assert_equal(min, field_stats.min)
+    assert_equal(max, field_stats.max)
+
+    assert_equal(number_data.tally.keys.size, field_stats.unique_count)
+    assert_equal(number_data.tally.keys.sort, field_stats.unique_values.sort)
+    assert_equal(number_data.tally, field_stats.frequencies)
+
+    mode = number_data.tally.max_by{ |k,v| v }.first
+    assert_equal(mode, field_stats.mode)
+  end
+
+  def test_unknown_type_stats
+    field_stats = ::FlatKit::FieldStats.new(name: "numeric-with-unknown")
+    field_stats, number_data = generate_data_with { Faker::Number.within(range: 1.0..100.0) }
+
+    unknown_count = 20
+    unknown_count.times {
+      field_stats.update("unknown")
+    }
+
+    refute(field_stats.field_type_determined?)
+
+    assert_equal(unknown_count, field_stats.unknown_count)
+    assert_equal(unknown_count + number_data.size, field_stats.total_count)
+    assert_in_epsilon(unknown_count.to_f / (unknown_count + number_data.size), field_stats.percent_unknown)
+  end
+
+  def test_resolves_type_automatically
+    field_stats = ::FlatKit::FieldStats.new(name: "numeric-autoresolve",guess_threshold: 101)
+    field_stats, _ = generate_data_with(stats: field_stats) { Faker::Number.within(range: 1.0..100.0) }
+
+    refute(field_stats.field_type_determined?)
+    field_stats, _ = generate_data_with(stats: field_stats) { Faker::Number.within(range: 200.0..300.0) }
+    assert(field_stats.field_type_determined?)
+  end
+
+  def test_resolves_integer_appropriately_with_mixed_data
+    field_stats = ::FlatKit::FieldStats.new(name: "numeric-integer",guess_threshold: 100)
+    field_stats, _ = generate_data_with(count: 40, stats: field_stats) { Faker::Number.within(range: 0..1).to_s }
+    field_stats, _ = generate_data_with(count: 70, stats: field_stats) { Faker::Number.within(range: 0..200).to_s }
+
+    assert_equal(::FlatKit::FieldType::IntegerType, field_stats.field_type)
+
+  end
+
+  def test_resolves_boolean_appropriately_with_mixed_data
+    field_stats = ::FlatKit::FieldStats.new(name: "numeric-integer",guess_threshold: 100)
+    field_stats, _ = generate_data_with(count: 70, stats: field_stats) { Faker::Boolean.boolean.to_s }
+    field_stats, _ = generate_data_with(count: 40, stats: field_stats) { Faker::Number.within(range: 0..200).to_s }
+    assert_equal(::FlatKit::FieldType::BooleanType, field_stats.field_type)
+  end
+
+  def test_resolves_string_appropriately_with_mixed_data
+    field_stats = ::FlatKit::FieldStats.new(name: "string",guess_threshold: 100)
+    field_stats, _ = generate_data_with(count: 61, stats: field_stats) { Faker::Color.name.to_s }
+    field_stats, _ = generate_data_with(count: 59, stats: field_stats) { Faker::Number.within(range: 0..200).to_s }
+    assert_equal(::FlatKit::FieldType::StringType, field_stats.field_type)
+
+    assert_equal(120, field_stats.count)
+    assert_equal(0, field_stats.unknown_count)
+    assert_equal(0, field_stats.null_count)
+  end
+end
